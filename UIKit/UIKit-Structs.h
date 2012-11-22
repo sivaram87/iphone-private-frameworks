21f14b5090d3ec8e0afaa7065dffc2ecfc4cf0d7
Sivaram <c.j.sivaram@gmail.com>
2012-11-22 19:40:07 +0530

    Revert "Eliminated warning on UIStatusBarData"
    This reverts commit 1c69952259628ff26c682a04b06166d9dd6e022e.

--- UIKit/UIKit-Structs.h
+++ UIKit/UIKit-Structs.h
@@ -114,8 +114,8 @@ typedef struct UIStatusBarData {
 	unsigned bluetoothConnected : 1;
 	unsigned displayRawGSMSignal : 1;
 	unsigned displayRawWifiSignal : 1;
-} UIStatusBarData;
-typedef UIStatusBarData XXStruct_AE4LvD;
+};
+typedef struct UIStatusBarData XXStruct_AE4LvD;
 
 @class UIView, UIImage;
 typedef union {
