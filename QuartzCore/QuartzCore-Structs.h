dab00faad9634f727cdf33ba7e2a93e2e0967eaa
Sivaram <c.j.sivaram@gmail.com>
2012-11-22 19:40:08 +0530

    Revert "Changes for IOSurface."
    This reverts commit 4737d8c2b0fa2b0d30125ccb6be0f1ce006dcfee.

--- QuartzCore/QuartzCore-Structs.h
+++ QuartzCore/QuartzCore-Structs.h
@@ -8,7 +8,6 @@
 #include <CoreGraphics/CoreGraphics.h>
 #include <QuartzCore/QuartzCore.h>
 #include <pthread.h>
-#include <IOSurface/IOSurface.h>
 
 typedef struct {
 	int x;
@@ -181,6 +180,8 @@ typedef struct EAGLNativeWindowCallbacksRec {
 typedef struct x_list_struct x_list_struct;
 
 typedef struct _CAImageQueue* CAImageQueueRef;
+
+typedef struct __IOSurface* IOSurfaceRef;
 
 @class CAEAGLLayer;
 typedef struct _CAEAGLNativeWindow {
