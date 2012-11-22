87e198ac4f00067dac31347786e2ba9575ba5d9c
Sivaram <c.j.sivaram@gmail.com>
2012-11-22 19:40:08 +0530

    Revert "CoreText (copying procedure only); UITwoSidedAlertController; Re-organizing liblockdown.h"
    This reverts commit 9d02662ec9fa9e53006559dacef1937131209ee7.

--- UIKit/UITwoSidedAlertController.h
+++ UIKit/UITwoSidedAlertController.h
@@ -5,11 +5,7 @@
 
 #import <Foundation/NSObject.h>
 
-@class UIAlertView, UITwoSidedAlertController;
-@protocol UITwoSidedAlertDelegate
-@optional
--(void)twoSidedAlertControllerDidDismiss:(UITwoSidedAlertController*)ctrler;
-@end
+@class UIAlertView;
 
 @interface UITwoSidedAlertController : NSObject {
 	UIAlertView* _front;
@@ -17,19 +13,15 @@
 	UIAlertView* _currentAlert;
 	id _delegate;
 }
--(void)setDelegate:(id<UITwoSidedAlertDelegate>)delegate;
--(UIAlertView*)frontAlert;
--(UIAlertView*)backAlert;
+-(void)dealloc;
+-(void)setDelegate:(id)delegate;
+-(id)frontAlert;
+-(id)backAlert;
+-(id)createFrontAlert;
+-(id)createBackAlert;
 -(void)show;
 -(void)dismiss;
 -(void)flip;
-@end
-
-
-@interface UITwoSidedAlertController ()
--(void)dealloc;
--(id)createFrontAlert;
--(id)createBackAlert;
 -(void)animationDidStop:(id)animation finished:(BOOL)finished;
 -(void)frontAlertClickedButtonAtIndex:(int)index;
 -(void)backAlertClickedButtonAtIndex:(int)index;
